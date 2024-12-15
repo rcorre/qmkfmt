366a8c390535d8e6e3b7d1322f7f44ad287f8500
Ryan Roden-Corrent <git@rrc.codes>
2024-12-15 09:35:10 -0500

    Format keys as a table.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use prettytable::{
-    format::{FormatBuilder, TableFormat},
-    Table,
-};
+use core::str;
+use prettytable::Table;
 use std::path::PathBuf;
 use streaming_iterator::StreamingIterator;
 
@@ -28,35 +26,53 @@ fn main() {
     let tree = parser.parse(&text, None).unwrap();
     let query = tree_sitter::Query::new(
         &language,
-        "(call_expression (identifier) @id (argument_list) @args)",
+        "(call_expression (identifier) @id (argument_list) @args) @call",
     )
     .unwrap();
+    let id_idx = query.capture_index_for_name("id").unwrap();
+    assert_eq!(id_idx, 0);
+    let args_idx = query.capture_index_for_name("args").unwrap();
+    assert_eq!(args_idx, 1);
+    let call_idx = query.capture_index_for_name("call").unwrap();
+    assert_eq!(call_idx, 2);
+    
+    
     let mut qc = tree_sitter::QueryCursor::new();
 
+    let column_count = 12;
+
+    let mut last_byte = 0;
     let mut it = qc.matches(&query, tree.root_node(), text.as_bytes());
     while let Some(m) = it.next() {
-        let node = m.captures[0].node;
-        let name = node
+        let indent = ""; // TODO: determine from initializer_pair
+        // Ensure this is a layout call, and if so, extract the name (e.g. LAYOUT_split_3x6_3)
+        let name = m.nodes_for_capture_index(id_idx).next().unwrap()
             .utf8_text(text.as_bytes())
             .expect("Failed to get text from node");
         if !name.starts_with("LAYOUT") {
             continue;
         }
-        eprintln!("{name}");
 
+        // Print everything before the call expression
+        let call_node = m.nodes_for_capture_index(call_idx).next().unwrap();
+        let prefix = &text.as_bytes()[last_byte..call_node.start_byte()];
+        let prefix = str::from_utf8(prefix).expect("Text is not utf-8");
+        last_byte = call_node.end_byte();
+        print!("{prefix}");
+
+        // Print the formatted key list inside parens
         let mut table = Table::new();
         table.set_format(*prettytable::format::consts::FORMAT_CLEAN);
         let node = m.captures[1].node;
         let mut qc = node.walk();
 
-        let keys: Vec<_> = m.captures[1]
-            .node
+        let keys: Vec<_> = m.nodes_for_capture_index(args_idx).next().unwrap()
             .named_children(&mut qc)
             .map(|node| node_to_text(&text, &node) + ",")
             .collect();
 
-        for row in keys.chunks(12) {
-            let fill = 12 - row.len();
+        for row in keys.chunks(column_count) {
+            let fill = column_count - row.len();
             table.add_row(
                 std::iter::repeat_n("", fill / 2)
                     .chain(row.iter().map(|s| s.as_str()))
@@ -65,8 +81,12 @@ fn main() {
             );
         }
 
-        println!("{}", table)
+        print!("{name}(\n{table}{indent})")
     }
+
+    let rest = &text.as_bytes()[last_byte..];
+    let rest = str::from_utf8(rest).expect("Text is not utf-8");
+    println!("{rest}");
 }
 
 fn node_to_text(text: &str, node: &tree_sitter::Node) -> String {
